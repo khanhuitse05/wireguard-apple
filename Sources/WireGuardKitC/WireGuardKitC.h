9aef50905c20ec9e598b9179b11a4065c754afe9
Walter Trần <walter.tran.goldenowl@gmail.com>
2026-08-03 17:46:15 +0700

    Update lib

--- Sources/WireGuardKitC/WireGuardKitC.h
+++ Sources/WireGuardKitC/WireGuardKitC.h
@@ -1,21 +1,21 @@
 // SPDX-License-Identifier: MIT
 // Copyright © 2018-2023 WireGuard LLC. All Rights Reserved.
 
-#import <Foundation/Foundation.h>
+#include <stdint.h>
 #include "key.h"
 #include "x25519.h"
 
 /* From <sys/kern_control.h> */
-#define CTLIOCGINFO 0xc0644e03UL
-struct ctl_info {
-    u_int32_t   ctl_id;
+#define WG_CTLIOCGINFO 0xc0644e03UL
+typedef struct wg_ctl_info {
+    uint32_t    ctl_id;
     char        ctl_name[96];
-};
-struct sockaddr_ctl {
-    u_char      sc_len;
-    u_char      sc_family;
-    u_int16_t   ss_sysaddr;
-    u_int32_t   sc_id;
-    u_int32_t   sc_unit;
-    u_int32_t   sc_reserved[5];
-};
+} wg_ctl_info;
+typedef struct wg_sockaddr_ctl {
+    uint8_t     sc_len;
+    uint8_t     sc_family;
+    uint16_t    ss_sysaddr;
+    uint32_t    sc_id;
+    uint32_t    sc_unit;
+    uint32_t    sc_reserved[5];
+} wg_sockaddr_ctl;
